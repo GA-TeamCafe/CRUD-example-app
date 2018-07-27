3feb26cf5b4c4f635b22ac4229150c5c968a29ce
beatrizelena <ortizwebdevelopment@gmail.com>
2018-07-27 13:31:13 -0400

    Fix URL in config.js
    https://wdi-library.herokuapp.com

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://wdi-library.herokuapp.com/books',
+  production: 'https://wdi-library.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
